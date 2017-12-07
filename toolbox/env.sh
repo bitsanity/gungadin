8436958ce50b9afac8e77b6426b3ca5fe5e6f434
bitsanity <brydiver@gmail.com>
2017-12-06 17:06:21 -0800

    .

--- toolbox/env.sh
+++
@@ -1,9 +0,0 @@
-export LD_LIBRARY_PATH=$HOME/secp256k1/.libs
-
-JLIB=-Djava.library.path=../jni
-
-JARS=\
-../lib/scrypt-1.4.0.jar:\
-../lib/zxing-core-3.2.1.jar:\
-../lib/zxing-javase-3.2.1.jar:\
-../lib/tbox.jar
--- toolbox/tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source env.sh
-
-java $JLIB -cp $JARS:.:./java tbox.Tests
